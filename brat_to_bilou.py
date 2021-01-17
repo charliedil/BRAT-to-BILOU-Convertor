f8450982532f301c1f3622cdc7b70424dcefe9b3
nehadil <nehadil2019@gmail.com>
2021-01-17 16:04:34 -0500

    first commit

---
+++ brat_to_bilou.py
@@ -0,0 +1,70 @@
+import argparse
+import nltk
+import os
+from nltk.tokenize import TreebankWordTokenizer as twt
+nltk.download("averaged_perceptron_tagger")
+def parse_entities(filepath):
+    entities = []
+    with open(filepath, "r") as entity_file:
+        lines = entity_file.read().split("\n")
+        for l in lines:
+            if l.startswith("T"):
+                temp = []
+                temp.append(l.split("\t")[2])
+                temp.append(l.split("\t")[1].split(" ")[0])
+                temp.append((l.split("\t")[1].split(" ")[1],l.split("\t")[1].split(" ")[2]))
+                entities.append(temp)
+    return entities
+
+parser = argparse.ArgumentParser(description="convert BRAT annotations to CONLL (BILOU)")
+parser.add_argument("input_dir", help="directory with input files and annotations files. should be in form of *.txt and *.ann, and every .txt file should have a .ann file with the same name, e.g. 101.txt and 101.ann")
+parser.add_argument("output_dir", help="directory to write output files to (.ann files in CONLL - BILOU format)")
+args = parser.parse_args()
+
+print("Checking input directory: "+args.input_dir)
+for filename in os.listdir(args.input_dir):
+    if filename.endswith(".txt") and not os.path.exists(args.input_dir+filename.split(".")[0]+".ann"):
+        print("No associated .ann file for file: "+filename)
+        exit()
+print("Directory passed check.")
+for filename in os.listdir(args.input_dir):
+    if filename.endswith(".txt"):
+        entities = parse_entities(args.input_dir+filename.split(".")[0]+".ann")
+        with open(args.input_dir+filename, "r") as f:
+            raw_text = f.read()
+            spans = list(twt().span_tokenize(raw_text))
+            tokenized_text = []
+            for s in spans:
+                tokenized_text.append(raw_text[s[0]:s[1]])
+            tagged_text = nltk.pos_tag(tokenized_text)
+            with open(args.output_dir+filename.split(".")[0]+".ann","w") as out:
+                bflag = False
+                for i in range(len(tagged_text)):
+                    out.write(tagged_text[i][0]+"\t"+tagged_text[i][1]+"\t")
+                    flag = True
+                    tracker = ""
+                    for e in entities:
+                        if int(e[2][0])-1 <= spans[i][0] and int(e[2][1])+1 >=spans[i][1]:
+                            if len(e[0].split(" "))==1 and (len(e[0])==1 or (len(e[0])!=1 and len(tagged_text[i][0])!=1)):
+                                out.write("U-"+e[1]+"\t"+str(spans[i][0])+" "+str(spans[i][1])+"\n")
+                                bflag = False
+                                flag=False
+                                break
+                            elif e[0].split(" ")[0] == tagged_text[i][0]:
+                                out.write("B-"+e[1]+"\t"+str(spans[i][0])+" "+str(spans[i][1])+"\n")
+                                tracker += tagged_text[i][0]
+                                flag=False
+                                bflag = True
+                                break
+                            elif e[0].split(" ")[len(e[0].split(" "))-1] == tagged_text[i][0] or e[0].replace(" ","")==tracker:
+                                out.write("L-"+e[1]+"\t"+str(spans[i][0])+" "+str(spans[i][1])+"\n")
+                                flag=False
+                                bflag=False
+                                break
+                            elif bflag == True:
+                                out.write("I-"+e[1]+"\t"+str(spans[i][0])+" "+str(spans[i][1])+"\n")
+                                tracker += tagged_text[i][0]
+                                flag=False
+                                break
+                    if flag:
+                        out.write("O"+"\t"+str(spans[i][0])+" "+str(spans[i][1])+"\n")
